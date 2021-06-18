2011b95ca4863f0f52981fe63867a5a428dcb3b2
fernandor-p <fernandor.p@hotmail.com>
2021-06-18 01:46:43 -0400

    fixed validation

--- src/js/component/secondscounter.js
+++ src/js/component/secondscounter.js
@@ -1,14 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-SecondsCounter.PropTypes = {
-	num1: PropTypes.number,
-	num2: PropTypes.number,
-	num3: PropTypes.number,
-	num4: PropTypes.number,
-	num5: PropTypes.number,
-	num6: PropTypes.number
-};
+//import PropTypes from "prop-types";
 
 function SecondsCounter(props) {
 	return (
@@ -27,5 +19,14 @@ function SecondsCounter(props) {
 		</div>
 	);
 }
+
+SecondsCounter.propTypes = {
+	num6: PropTypes.any,
+	num5: PropTypes.any,
+	num4: PropTypes.any,
+	num3: PropTypes.any,
+	num2: PropTypes.any,
+	num1: PropTypes.any
+};
 
 export default SecondsCounter;
